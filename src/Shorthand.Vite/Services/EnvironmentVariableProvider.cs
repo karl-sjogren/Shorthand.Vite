c997055b51b897b9df34f568c02e14f98495edde
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2023-07-03 23:38:19 +0200

    Added comment and excluded EnvironmentVariableProvider from coverage

--- src/Shorthand.Vite/Services/EnvironmentVariableProvider.cs
+++ src/Shorthand.Vite/Services/EnvironmentVariableProvider.cs
@@ -2,6 +2,10 @@ using Shorthand.Vite.Contracts;
 
 namespace Shorthand.Vite.Services;
 
+// The whole idea of this abstraction is to make environment variable
+// stuff more testable, this however makes this class almost
+// untestable, so we exclude it from code coverage.
+[ExcludeFromCodeCoverage]
 internal class EnvironmentVariableProvider : IEnvironmentVariableProvider {
     public string? GetEnvironmentVariable(string variable) {
         return Environment.GetEnvironmentVariable(variable);
